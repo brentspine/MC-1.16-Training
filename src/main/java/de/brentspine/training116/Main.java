14780d6275b5eac6a964e0b8b094c16eb300af44
youbo <stolze.luca@gmail.com>
2022-02-14 21:20:21 +0100

    Edit plugin.yml and register commands

--- src/main/java/de/brentspine/training116/Main.java
+++ src/main/java/de/brentspine/training116/Main.java
@@ -1,6 +1,10 @@
 package de.brentspine.training116;
 
+import de.brentspine.training116.commands.SetupCommand;
+import de.brentspine.training116.listeners.PlayerJoinListener;
+import de.brentspine.training116.listeners.items.GameSelector;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -13,7 +17,14 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
+        init(Bukkit.getPluginManager());
         Bukkit.broadcastMessage(Main.PREFIX + "§aHello World");
+    }
+
+    public void init(PluginManager pluginManager) {
+        getCommand("setup").setExecutor(new SetupCommand(this));
+        pluginManager.registerEvents(new PlayerJoinListener(), this);
+        pluginManager.registerEvents(new GameSelector(), this);
     }
 
     @Override
